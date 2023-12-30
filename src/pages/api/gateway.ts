6c55a6090f4145ad4faa0941a64d297e36d23b99
arvinxx <arvinx@foxmail.com>
2023-12-30 22:56:01 +0800

    :green_heart: ci: fix ci

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -1,9 +1,5 @@
 import { createLobeChatPluginGateway } from '@lobehub/chat-plugins-gateway';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default async (req: Request) => {
   if (process.env.NODE_ENV === 'development') {
     return createLobeChatPluginGateway()(req);
