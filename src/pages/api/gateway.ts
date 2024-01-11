8b1a443e7fd43061168686a8727aeee426fcfd1d
Arvin Xu <arvinx@foxmail.com>
2024-01-11 12:51:52 +0800

    :bug: fix: fix gateway runtime (#24)

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -1,9 +1,13 @@
-import { createLobeChatPluginGateway } from '@lobehub/chat-plugins-gateway';
+import { createGatewayOnEdgeRuntime } from '@lobehub/chat-plugins-gateway';
+
+export const config = {
+  runtime: 'edge',
+};
 
 export default async (req: Request) => {
   if (process.env.NODE_ENV === 'development') {
-    return createLobeChatPluginGateway()(req);
+    return createGatewayOnEdgeRuntime()(req);
   }
 
-  return;
+  return new Response('gateway');
 };
